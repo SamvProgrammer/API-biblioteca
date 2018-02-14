4816af25b3ad3593ec1dd0baee979f62f0f703fd
santiago antonio <santiago antonio@santiagoantonio>
2018-02-13 20:01:42 -0600

    autentificacion

--- src/main/java/com/biblioteca/controllers/login.java
+++ src/main/java/com/biblioteca/controllers/login.java
@@ -7,6 +7,9 @@ package com.biblioteca.controllers;
 
 import com.biblioteca.dao.usuarioDao;
 import com.biblioteca.repositorios.usuarios;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -16,8 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
- * @author santiago antonio
- * repositorio en git
+ * @author santiago antonio repositorio en git
  */
 @RestController
 public class login {
@@ -26,10 +28,64 @@ public class login {
     @Qualifier("objUsuarioDao")
     private usuarioDao usuarios;
 
+    private ArrayList<Map<String, Object>> logeosTotales = new ArrayList<Map<String, Object>>();
+
     @PostMapping("/login")
     public Object getLoginUser(@RequestBody usuarios obj, HttpServletRequest request) {
-        Object respuesta = usuarios.obtenerLogin(obj);
+        Map<String, Object> respuesta = (Map<String, Object>) usuarios.obtenerLogin(obj);
+        if ((boolean) (respuesta.get("acceso")) == true) {
+            boolean repetido = false;
+            for (Map<String, Object> item : logeosTotales) {
+                if (item.get("codigo").equals(respuesta.get("codigo"))) {
+                    respuesta = new HashMap<String, Object>();
+                    respuesta.put("respuesta", "Usuario ya activo en otra sesión.");
+                    respuesta.put("acceso", false);
+                    repetido = true;
+                    break;
+                }
+            }
+            if (!repetido) {
+                logeosTotales.add(respuesta);
+            }
+        }
         return respuesta;
     }
 
+    @PostMapping("/logout")
+    public Object logOut(@RequestBody usuarios obj, HttpServletRequest request) {
+        Map<String, Object> respuesta = new HashMap<String, Object>();
+        Map<String, Object> temporal = null;
+        boolean eliminar = false;
+        for (Map<String, Object> item : logeosTotales) {
+            if (item.get("codigo").equals(obj.getCodigo())) {
+                temporal = item;
+                eliminar = true;
+                respuesta.put("respuesta", "Cerrando sesión");
+                respuesta.put("acceso", false);
+                break;
+            }
+        }
+        if (eliminar) {
+            logeosTotales.remove(temporal);
+        }
+        return respuesta;
+    }
+
+    @PostMapping("/autentificando")
+    public Object autentificado(@RequestBody usuarios obj, HttpServletRequest request) {
+        Map<String, Object> respuesta = new HashMap<String, Object>();
+        if (logeosTotales.size() != 0) {
+            for (Map<String, Object> item : logeosTotales) {
+                if (item.get("codigo").equals(obj.getCodigo())) {
+                    respuesta.put("acceso", true);
+                    respuesta.put("roles", item.get("roles"));
+                } else {
+                    respuesta.put("acceso", false);
+                }
+            }
+        } else {
+            respuesta.put("acceso", false);
+        }
+        return respuesta;
+    }
 }
