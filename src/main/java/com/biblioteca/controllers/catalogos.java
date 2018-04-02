95cb1f08423d5624d17ca8146f7588246feea4ea
Samv <santiagoantoniomariscal@gmail.com>
2018-04-02 08:52:08 -0500

    apartado y pedido primera parte

--- src/main/java/com/biblioteca/controllers/catalogos.java
+++ src/main/java/com/biblioteca/controllers/catalogos.java
@@ -2,9 +2,9 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ probando
  */
 package com.biblioteca.controllers;
-
 import com.biblioteca.dao.editorialDao;
 import com.biblioteca.dao.generosDao;
 import com.biblioteca.dao.librosDao;
